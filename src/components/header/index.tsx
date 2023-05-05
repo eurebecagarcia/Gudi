d5ec7c93e6fa56e3f709911e05c68be515c30041
Rebeca Garcia <rebecagarciarodrigues21@gmail.com>
2023-05-05 17:19:09 -0300

    adjusting responsive menu

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -2,34 +2,27 @@ import Button from "../button";
 import Logo from "../../assets/logo.png";
 import "./header.scss";
 import { AiOutlineCloseCircle, AiOutlineMenu } from "react-icons/ai";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function Header() {
+  const [isMobile, setIsMobile] = useState(false);
   const [openMenu, setOpenMenu] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (window.innerWidth <= 500) {
+      setIsMobile(true);
+      console.log("mobile", isMobile);
+    } else {
+      setIsMobile(false);
+      console.log("mobile", isMobile);
+    }
+  });
 
   return (
     <header>
       <img src={Logo} alt="logo Gudi" />
-      {!openMenu && window.innerWidth <= 500 ? (
-        <AiOutlineMenu
-          onClick={() => setOpenMenu(true)}
-          color="var(--main-blue)"
-          size={30}
-        />
-      ) : (
+      {!isMobile ? (
         <nav className="menu">
-          {window.innerWidth <= 500 && (
-            <AiOutlineCloseCircle
-              color="var(--main-blue)"
-              size={40}
-              onClick={() => setOpenMenu(false)}
-              style={{
-                position: "absolute",
-                top: "25px",
-                right: "25px",
-              }}
-            />
-          )}
           <ul>
             <li onClick={() => setOpenMenu(false)}>Sobre</li>
             <li onClick={() => setOpenMenu(false)}>Benefícios</li>
@@ -39,6 +32,35 @@ export default function Header() {
             </li>
           </ul>
         </nav>
+      ) : openMenu ? (
+        <nav className="menu-mobile">
+          <AiOutlineCloseCircle
+            color="var(--main-blue)"
+            size={40}
+            onClick={() => setOpenMenu(false)}
+            style={{
+              position: "absolute",
+              top: "25px",
+              right: "25px",
+              zIndex: 2,
+            }}
+          />
+          <ul className="menu-mobile">
+            <li></li>
+            <li onClick={() => setOpenMenu(false)}>Sobre</li>
+            <li onClick={() => setOpenMenu(false)}>Benefícios</li>
+            <li onClick={() => setOpenMenu(false)}>Contato</li>
+            <li onClick={() => setOpenMenu(false)}>
+              <Button text="agendar" />
+            </li>
+          </ul>
+        </nav>
+      ) : (
+        <AiOutlineMenu
+          onClick={() => setOpenMenu(true)}
+          color="var(--main-blue)"
+          size={30}
+        />
       )}
     </header>
   );
