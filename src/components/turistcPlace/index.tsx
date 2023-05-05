f387da2b64df625df1fef842222594779f77afc9
Rebeca Garcia <rebecagarciarodrigues21@gmail.com>
2023-05-05 01:18:53 -0300

    remove unnecessary imports

--- src/components/turistcPlace/index.tsx
+++ src/components/turistcPlace/index.tsx
@@ -1,15 +1,9 @@
 import "./turistcPlace.scss";
-import Rj from "../../assets/rJ.svg";
-import Ro from "../../assets/rO.svg";
-import Cn from "../../assets/Cn.svg";
-import Am from "../../assets/aM.svg";
-import Button from "../button";
 
 export default function TuristcPlaces() {
   return (
     <section className="container-places">
       <div className="rio-de-janeiro">
-        {/* <img src={Rj} alt="" /> */}
         <button>Rio de Janeiro</button>
       </div>
       <div className="rio-das-ostras">
